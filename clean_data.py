0f8fcdd445c81260f679dd0fe397aab543926955
Mart Roben <mart.roben@gmail.com>
2023-09-23 22:18:13 +0300

    Simplified syntax and naming

--- clean_data.py
+++ clean_data.py
@@ -28,7 +28,14 @@ publications_raw_table = "PublicationRaw"
 
 sql_connection = sql_operations.get_connection(database_path)
 authors_sql = "SELECT Guid, Authors, AuthorsText FROM PublicationRaw"
-publication_authors = sql_connection.cursor().execute(authors_sql).fetchall()
+publications_raw = sql_connection.cursor().execute(authors_sql).fetchall()
+
+publications = list()
+for id, authors_data, authors_text in publications_raw:
+    publications += [dict(
+        id=id,
+        authors_data=json.loads(authors_data),
+        authors_text=authors_text)]
 
 
 #####################################
@@ -92,7 +99,7 @@ patterns_detect = [
     (last_first_single, last_first)]
 
 
-authors_cleaner = data_operations.AuthorsCleaner(
+author_cleaner = data_operations.AuthorCleaner(
     delimiter=";",
     unwanted_substrings=unwanted_substrings)
 
@@ -104,38 +111,38 @@ author_parser = data_operations.AuthorParser(
 globals()["log_latinized"] = list()
 globals()["log_parse_fail"] = list()
 
-publication_authors_parsed = list()
-for id, authors, authors_string in publication_authors:
+for pub in publications:
     # clean for splitting
-    authors_string_cleaned = authors_cleaner.clean_delimiter(authors_string)
+    text_cleaned = author_cleaner.clean_delimiter(pub["authors_text"])
     # latinize
-    authors_latinized = authors_cleaner.latinize(authors_string_cleaned)
-    if authors_latinized != authors_string_cleaned and "log_latinized" in globals():
-        globals()["log_latinized"] += [(id, authors_string_cleaned, authors_latinized)]
+    text_latinized = author_cleaner.latinize(text_cleaned)
+    if text_latinized != text_cleaned and "log_latinized" in globals():
+        globals()["log_latinized"] += [(id, text_cleaned, text_latinized)]
     # split
-    authors_split = authors_cleaner.split(authors_latinized)
+    authors_split = author_cleaner.split(text_latinized)
     # remove unwanted substrings
-    authors_substrings_removed = [authors_cleaner.remove_substrings(author) for author in authors_split]
+    authors_substrings_removed = [author_cleaner.remove_substrings(author) for author in authors_split]
     # clean split strings
-    authors_cleaned = [authors_cleaner.clean(author) for author in authors_substrings_removed]
+    authors_cleaned = [author_cleaner.clean(author) for author in authors_substrings_removed]
 
     # parse authors
     exact_matches = [author_parser.check_exact_match(author) for author in authors_cleaned]
     authors_parsed = [author for (author, is_exact_match) in zip(authors_cleaned, exact_matches) if is_exact_match]
     if not_parsed_authors := [author for (author, is_exact_match) in zip(authors_cleaned, exact_matches) if not is_exact_match]:
-        for author in not_parsed_authors:
-            successful_parse = author_parser.parse_bad_delimiter(author)
+        for non_standard_author in not_parsed_authors:
+            successful_parse = author_parser.parse_bad_delimiter(non_standard_author)
             if not successful_parse:
                 parsed_authors = list()
                 break
             authors_parsed += successful_parse
     if not authors_parsed and "log_parse_fail" in globals():
-        globals()["log_parse_fail"] += [(authors_string, authors_cleaned)]
-    publication_authors_parsed += [(id, authors, authors_parsed)]
+        globals()["log_parse_fail"] += [(pub["authors_text"], authors_cleaned)]
+    # Add a new key with the result
+    pub["authors_parsed"] = authors_parsed
 
 # Log relevant information
 log.latinized(globals().get("log_latinized"), logging.getLogger("etis"))
-total_entries = len(publication_authors_parsed)
+total_entries = len(publications)
 log.parse_fail(total_entries, globals().get("log_parse_fail"), logging.getLogger("etis"))
 
 
@@ -143,6 +150,7 @@ log.parse_fail(total_entries, globals().get("log_parse_fail"), logging.getLogger
 # Standardize parsed names #
 ############################
 
+# Bring to unified format: 'I. Name' or 'First Last'
 name_initial_groups = rf"(?P<last>({prefix})?\s?{name})[,\s]\s*(?P<first>{initial})"
 initial_name_groups = rf"(?P<first>{initial})[,\s]*\s*(?P<last>({prefix})?\s?{name})"
 last_first_groups = rf"(?P<last>({prefix})?\s?{name}),\s*(?P<first>{name}(\s*{name})?)"
@@ -150,20 +158,20 @@ patterns_standardize = [name_initial_groups, initial_name_groups, last_first_gro
 
 author_standardizer = data_operations.AuthorStandardizer(patterns_standardize, initial)
 
+for pub in publications:
+    pub["authors_clean"] = [author_standardizer.standardize(parsed_author) for parsed_author in pub["authors_parsed"]]
+
+
+######################
+# Levenshtein scores #
+######################
+
+
+
+
 ####################### Could use some Pandas mutate equivalent?
 ####################### Or just make into a dict and modify in place?
 
-publication_authors_standardized = list()
-for id, authors, authors_parsed in publication_authors_parsed:
-    authors_parsed_new = [author_standardizer.standardize(author) for author in authors_parsed]
-    publication_authors_standardized += [(id, authors, authors_parsed_new)]
-
-publication_authors_json_parsed = list()
-for id, authors, authors_parsed in publication_authors_standardized:
-    publication_authors_json_parsed += [
-        (id,
-         json.loads(authors),
-         authors_parsed)]
 
 publication_authors_match_scores = list()
 for id, authors, authors_parsed in publication_authors_json_parsed:
